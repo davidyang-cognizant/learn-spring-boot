ad71de286b03ccb4af405e76e4f7cfd7e7b6cd32
David <yangda@msoe.edu>
2022-09-21 11:42:05 -0500

    Initial commit

---
+++ src/main/java/com/example/learnspringboot/CourseController.java
@@ -0,0 +1,23 @@
+package com.example.learnspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @GetMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "in28minutes"),
+                new Course(2, "Learn DevOps", "in28minutes"),
+                new Course(3, "Learn Azure", "in28minutes"),
+                new Course(4, "Learn Firebase", "in28minutes")
+        );
+    }
+
+}
---
+++ src/main/java/com/example/learnspringboot/CurrencyConfigurationController.java
@@ -0,0 +1,21 @@
+package com.example.learnspringboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CurrencyConfigurationController {
+
+    @Autowired
+    private CurrencyServiceConfiguration configuration;
+
+    @GetMapping("/currency-configuration")
+    public CurrencyServiceConfiguration retrieveCurrencyService() {
+        return configuration;
+    }
+
+}
